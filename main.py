228146938ecb0ae5ba693ce75f88b3464927bcac
notBenjii <301025419+notBenjii@users.noreply.github.com>
2026-07-08 14:18:06 +0200

    Initial commit: add Weather API response and gitignore

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from keys import API_KEY
+
+city = ""
+
+while not city:
+    city_chosen = input("City name: ")
+    if city_chosen:
+        city = city_chosen
+    else:
+        print("Please enter a valid city name.")
+
+url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
+
+response = requests.get(url)
+
+data = response.json()
+
+date = data["current"]["last_updated"]
+temp = data["current"]["temp_c"]
+condition = data["current"]["condition"]["text"]
+wind = data["current"]["wind_kph"]
+cloud_percentage = data["current"]["cloud"]
+
+print(f"Last updated {date}")
+print(f"Temperature: {temp}°C")
+print(f"Condition: {condition}")
+print(f"Wind: {wind} kph")
+print(f"Cloud coverage: {cloud_percentage}%")
