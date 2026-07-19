740bb5ca18b1d62d427f121058b1cd5b44f593aa
notBenjii <301025419+notBenjii@users.noreply.github.com>
2026-07-20 01:45:24 +0200

    Turn main.py into a continuous loop with quit and error handling

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from cache import WeatherCache
 from request import get_weather
 
-city = ""
-while not city:
-    city = input("City name: ").strip().lower()
-    if not city:
-        print("Please enter a valid city name.")
-
 cache = WeatherCache()
-data = cache.get(city)
-if data is None:
-    data = get_weather(city)
-    if data is not None:
-        cache.set(city, data)
 
-date = data["current"]["last_updated"]
-temp = data["current"]["temp_c"]
-condition = data["current"]["condition"]["text"]
-wind = data["current"]["wind_kph"]
-cloud_percentage = data["current"]["cloud"]
+while True:
+    city = ""
+    while not city:
+        print("Please enter a city, or type 'q' to quit")
+        city = input("City name: ").strip().lower()
+        if not city:
+            print("Please enter a valid city name.")
 
-print(f"Last updated {date}")
-print(f"Temperature: {temp}°C")
-print(f"Condition: {condition}")
-print(f"Wind: {wind} kph")
-print(f"Cloud coverage: {cloud_percentage}%")
+    if city == "q":
+        break
+
+    data = cache.get(city)
+    if data is None:
+        data = get_weather(city)
+        if data is not None:
+            cache.set(city, data)
+        else:
+            continue
+
+    date = data["current"]["last_updated"]
+    temp = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+    wind = data["current"]["wind_kph"]
+    cloud_percentage = data["current"]["cloud"]
+
+    print(f"Last updated {date}")
+    print(f"Temperature: {temp}°C")
+    print(f"Condition: {condition}")
+    print(f"Wind: {wind} kph")
+    print(f"Cloud coverage: {cloud_percentage}%")
+    print()
